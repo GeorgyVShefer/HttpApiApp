5bf9eb71ee291e76535b9f20e666e2e0bb98feda
GeorgyVShefer <156106194+GeorgyVShefer@users.noreply.github.com>
2026-01-18 19:12:10 +0300

    init

---
+++ src/test/java/org/example/httpapiapp/HttpApiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.httpapiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpApiAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
